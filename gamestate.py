f002f9d81aa5a8d65c4de2bf7be47b98725aa438
Milo <milo.grv@gmail.com>
2025-12-04 09:54:47 +0100

    WIP: added Player class in gamestate

--- gamestate.py
+++ gamestate.py
@@ -1,14 +1,22 @@
 import time
 from plateau import *
+from uuid import uuid4
+
+class Player:
+    def __init__(self, name: str):
+        self.id = str(uuid4())
+        self.name: str = name
+        self.ships: list[Boat] = []
+        self.plateau: Plateau | None = None
 
 class GameState:
-    def __init__(self, p1_name: str, p2_name: str, plateau_size: tuple[int, int, int] = (7, 5, 3)):
-        self.p1: str = p1_name
-        self.p2: str = p2_name
+    def __init__(self, p1: Player, p2: Player, plateau_size: tuple[int, int, int] = (7, 5, 3)):
+        self.p1: Player = p1
+        self.p2: Player = p2
         
         # Init both players empty plateau
-        self.p1_plateau = Plateau(*plateau_size)
-        self.p2_plateau = Plateau(*plateau_size)
+        self.p1.plateau = Plateau(*plateau_size)
+        self.p2.plateau = Plateau(*plateau_size)
 
         # Timestamps
         self.started_at: float = time.time()
@@ -16,14 +24,14 @@ class GameState:
 
         # Miscs
         self.turns = 0
-        self.current_turn: str = p1_name
+        self.current_turn: str = p1.id
         self.phase: str = 'placement'
         self.winner: str | None = None
 
     def switch_turn(self):
         """Switch the current turn to the other player."""
-        if self.current_turn == self.p1:
-            self.current_turn = self.p2
+        if self.current_turn == self.p1.id:
+            self.current_turn = self.p2.id
         else:
-            self.current_turn = self.p1
+            self.current_turn = self.p1.id
         self.turns += 1
