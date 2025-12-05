2e6a7355b9f9f2ce11c1f1d8dad67b114ee606f1
RobinMartineau <robin.martineau09@gmail.com>
2025-12-05 16:05:10 +0100

    AJout de brrrla et charge

--- music.py
+++ music.py
@@ -1,1 +1,33 @@
 import pygame
+from time import sleep
+
+def init_music(start:float, music:str):
+    pygame.mixer.init()
+    pygame.mixer.music.load(music) 
+    pygame.mixer.music.set_volume(0.5)
+    pygame.mixer.music.play(loops=0, start=start)
+
+def BRRRRRLAA():
+    """
+    BBBBBRRRRRRR LAA
+    """
+    init_music(45.9,"musics/charge.mp3")
+    sleep(1.5)
+    stop_music()
+
+def charge():
+    """
+    CHARGEE ! CHARGEEE ! 
+    """
+    init_music(68.7,"musics/charge.mp3")
+    sleep(0.9)
+    stop_music()
+
+
+def stop_music():
+    pygame.mixer.music.stop()
+
+if __name__ == "__main__":
+    while(1):
+        BRRRRRLAA()
+        charge()
